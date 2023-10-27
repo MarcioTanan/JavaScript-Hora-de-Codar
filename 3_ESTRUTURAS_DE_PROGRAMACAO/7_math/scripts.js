27d1f98e7491f5b4e35da5254f2a2c0d8ed474d7
Marcio Tanan <marcio-tanan@hotmail.com>
2023-10-27 09:29:35 -0300

    update

---
+++ 3_ESTRUTURAS_DE_PROGRAMACAO/7_math/scripts.js
@@ -0,0 +1,15 @@
+let maior = Math.max(6,12,28,55,8)
+
+console.log(maior)
+
+let menor = Math.min(6,12,28,55,8)
+
+console.log(menor)
+
+let arredondar = Math.round(7.44165461)
+
+console.log(arredondar)
+
+let arredondarpracima = Math.ceil(7.26849864)
+
+console.log(arredondarpracima)

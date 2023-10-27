919465809c4c9fd24e00caeeea179a56b92d81ee
Marcio Tanan <marcio-tanan@hotmail.com>
2023-10-27 08:50:04 -0300

    update

---
+++ 3_ESTRUTURAS_DE_PROGRAMACAO/1_variaveis/scripts.js
@@ -0,0 +1,18 @@
+let nome = "Marcio"
+
+console.log(nome)
+console.log(`O meu nome é ${nome}`)
+
+let laranjas = 5
+console.log(laranjas * laranjas)
+
+laranjas = laranjas + 1
+console.log(laranjas)
+
+nome = "João"
+
+console.log(`O meu nome é ${nome}`)
+
+laranjas = "Laranja"
+
+console.log(laranjas)
---
+++ 3_ESTRUTURAS_DE_PROGRAMACAO/2_variaveis_const_var/script.js
@@ -0,0 +1,13 @@
+var nome = "Marcio"
+const ip = "123.0.0.1"
+
+console.log(nome)
+console.log(ip)
+
+nome = "João"
+console.log(nome)
+
+
+ip = "127.1.1.1"
+console.log(ip)
+

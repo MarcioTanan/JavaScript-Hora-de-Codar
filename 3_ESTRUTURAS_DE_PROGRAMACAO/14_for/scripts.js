af056be8535e1210b8507b508b47eb5d66d9e905
Marcio Tanan <marcio-tanan@hotmail.com>
2023-10-30 09:24:45 -0300

    update

---
+++ 3_ESTRUTURAS_DE_PROGRAMACAO/14_for/scripts.js
@@ -0,0 +1,9 @@
+for(let i = 0; i < 100; i = i + 3){
+  console.log(`A soma de ${i} com 2 é ${i + 2}`)
+}
+let x = 0
+
+while(x < 100){
+  console.log(`A soma de ${x} com 2 é ${x + 2}`)
+  x = x + 3
+}

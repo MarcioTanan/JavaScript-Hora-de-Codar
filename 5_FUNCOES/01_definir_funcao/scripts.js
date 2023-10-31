4316fc69a3bdd3146379792b4cc79e1ebf87b8ef
Marcio Tanan <marcio-tanan@hotmail.com>
2023-10-31 08:59:34 -0300

    update

---
+++ 5_FUNCOES/01_definir_funcao/scripts.js
@@ -0,0 +1,23 @@
+function imprimirNoConsole(){
+  console.log("Olá, mundo!")
+}
+
+imprimirNoConsole()
+
+
+function imprimirUmNumero(num){
+  console.log("O número é: " + num)
+}
+
+imprimirUmNumero(100)
+imprimirUmNumero(10)
+imprimirUmNumero(20)
+imprimirUmNumero(30)
+
+const numeroAleatorio = function(){
+  console.log(Math.random())
+}
+
+numeroAleatorio()
+numeroAleatorio()
+numeroAleatorio()

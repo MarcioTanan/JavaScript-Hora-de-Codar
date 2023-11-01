175500d9771ba57481f97003647a40c2512d58e9
Marcio Tanan <marcio-tanan@hotmail.com>
2023-11-01 10:27:41 -0300

    update

---
+++ 6_EXERCICIOS/8_exercicio_23/scripts.js
@@ -0,0 +1,10 @@
+function texto(x){
+  if(x.length > 10){
+    console.log("Texto muito longo")
+  }else{
+    console.log("Texto dentro do limite")
+  }
+}
+
+texto("marcio alves de oliveira cerqueira tanan")
+texto("Marcio")
---
+++ 6_EXERCICIOS/9_exercicio_24/scripts.js
@@ -0,0 +1,7 @@
+function numb(x, y){
+  return Math.pow(x,y)
+}
+
+console.log(numb(3,2))
+console.log(numb(4,2))
+console.log(numb(3,3))

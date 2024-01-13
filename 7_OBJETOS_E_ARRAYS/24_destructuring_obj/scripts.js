77a443560455ece655a967913009947c6528c01c
Marcio Tanan <marcio-tanan@hotmail.com>
2024-01-13 17:56:14 -0300

    update

--- 7_OBJETOS_E_ARRAYS/24_destructuring_obj/scripts.js
+++ 7_OBJETOS_E_ARRAYS/24_destructuring_obj/scripts.js
@@ -7,3 +7,17 @@ const {name: fname, lastname:lname} = person;
 
 console.log(fname)
 console.log(lname)
+
+let obj = {
+    rodas: 4,
+    portas: 4,
+    tetosolar:true,
+    motor: '2.0'
+}
+
+const {rodas:vRodas, portas:vPortas, tetosolar:vTetosola, motor:vMotor} = obj
+
+console.log(vRodas)
+console.log(vPortas)
+console.log(vTetosola)
+console.log(vMotor)

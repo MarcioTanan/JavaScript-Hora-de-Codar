b7ac19298b4969231d920d17ca28b318b183a181
Marcio Tanan <marcio-tanan@hotmail.com>
2024-01-14 11:12:47 -0300

    update

--- 8_EXERCICIOS/04_exercicio_29/scripts.js
+++ 8_EXERCICIOS/04_exercicio_29/scripts.js
@@ -4,13 +4,8 @@ let onibus = {
     portas:2
 }
 
-let onibusB = {
-    janelas:20
-}
-
-Object.assign(onibus,onibusB)
-
 delete onibus.rodas
-
-console.log(onibus)
 console.log(onibus.rodas)
+
+onibus.janelas = 20
+console.log(onibus)

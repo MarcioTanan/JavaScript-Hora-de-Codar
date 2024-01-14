0fb1cb747f464618881836e131f2df22ad7b0465
Marcio Tanan <marcio-tanan@hotmail.com>
2024-01-14 11:47:26 -0300

    update

---
+++ 8_EXERCICIOS/08_exercicio_33/scripts.js
@@ -0,0 +1,9 @@
+let carro = {
+    "portas": 4,
+    "motor": 2.0,
+    "ano": 2015
+}
+
+console.log(carro.portas)
+console.log(carro.motor)
+console.log(carro.ano)
---
+++ 8_EXERCICIOS/09_exercicio_34/scripts.js
@@ -0,0 +1,9 @@
+let nome = "Marcio Alves de Oliveira Cerqueira Tanan"
+
+let palavras = nome.split(' ')
+
+console.log(palavras)
+
+for(let i = 0; i < palavras.length;i++){
+    console.log(palavras[i])
+}

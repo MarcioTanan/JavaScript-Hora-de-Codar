0297a20515718ccc7a11ab1df817bb09da55dfac
Marcio Tanan <marcio-tanan@hotmail.com>
2024-01-16 10:48:26 -0300

    update

---
+++ 9_CONCEITOS_DE_OO/10_class_methods/scripts.js
@@ -0,0 +1,17 @@
+class Cachorro2 {
+    constructor(raca,cor){
+        this.raca = raca
+        this.cor = cor
+    }
+    latir(){
+        console.log("Au Au Au")
+    }
+}
+
+Cachorro2.prototype.patas = 4
+
+let doberman = new Cachorro2('Doberman','preto')
+
+console.log(doberman)
+console.log(doberman.patas)
+doberman.latir()

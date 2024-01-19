3d41dc335735c5cb02adcc6b12868dbfd1fb02f2
Marcio Tanan <marcio-tanan@hotmail.com>
2024-01-19 10:55:04 -0300

    update

---
+++ 11_DEBUGS_E_ERROS/05_exception/scripts.js
@@ -0,0 +1,13 @@
+function saudacao(nome) {
+  if (isNaN(nome2)) {
+  alert(`Olá ${nome}.`);
+} else {
+  throw new Error("Você digitou um número. O nome precisa ser uma string.")
+  }
+}
+let nome2 = prompt("Qual seu nome?")
+
+saudacao(nome2)
+
+console.log("teste");
+

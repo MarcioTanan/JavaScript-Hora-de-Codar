df84a1a52763eec36c71d199cb47b4599ea5b32d
Marcio Tanan <marcio-tanan@hotmail.com>
2024-01-23 17:27:53 -0300

    update

---
+++ 12_EXPRESSOES_REGULARES/06_operador_plus/scripts.js
@@ -0,0 +1,7 @@
+const umOumaisNumeros = /\d+/
+
+console.log(umOumaisNumeros.test("1518"))
+console.log(umOumaisNumeros.test(""))
+console.log(umOumaisNumeros.test("asdasd"))
+console.log(umOumaisNumeros.test("1"))
+console.log(umOumaisNumeros.test("16516548654186465161"))
